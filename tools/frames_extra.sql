2282458f3bbb3c58fa329832a59a596b1520580a
1313ou <1313ou@gmail.com>
2021-11-03 08:03:10 +0100

    cleanup

--- tools/frames_extra.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO vvframes VALUES (36,'Somebody ----s at something');
-INSERT INTO vvframes VALUES (37,'Somebody ----s for something');
-INSERT INTO vvframes VALUES (38,'Somebody ----s on somebody');
-INSERT INTO vvframes VALUES (39,'Somebody ----s out of somebody');
-
